212b7a61d688247651c0161a4fcaa30e6dc727ba
Lauvizh <webdev@loicfontaine.net>
2016-10-02 14:18:22 +0200

    sonataAdminBundle installé vide

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,7 +15,18 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            // Pagination
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+
+            // Sonata Administration manager
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+
+            // App
             new AppBundle\AppBundle(),
         ];
 
