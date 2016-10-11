84626d1d6b774c38ac9647e579d5b27cbfdeebba
Lauvizh <webdev@loicfontaine.net>
2016-10-11 22:38:32 +0200

    ajout des type de champs pour les visages

--- src/AppBundle/Form/FaceType.php
+++ src/AppBundle/Form/FaceType.php
@@ -4,7 +4,8 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class FaceType extends AbstractType
@@ -16,10 +17,14 @@ class FaceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('birthDate', DateTimeType::class)
-        ;
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('birthDate', BirthdayType::class, array(
+                    'widget' => 'single_text',
+                    'html5' => false,
+                    'attr' => ['class' => 'js-datepicker'],
+                    ))
+                ;
     }
     
     /**
